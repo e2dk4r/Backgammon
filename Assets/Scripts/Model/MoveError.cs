7c9d64d79bc9fe7f88844fb10a7ab0f37bbb7db3
e2dk4r <43293320+e2dk4r@users.noreply.github.com>
2020-03-16 12:55:00 +0300

    add error of moving outside

--- Assets/Scripts/Model/MoveError.cs
+++ Assets/Scripts/Model/MoveError.cs
@@ -1,11 +1,17 @@
-﻿public enum MoveError
+public enum MoveError
 {
     // no error
     NoError,
+
+    // you have to move counter-clockwise
+    WrongHomeDirection,
     // there is not enough step value to move in
     NotEnoughSteps,
+    // there is more than one enemy piece in slot
+    BlockedByEnemy,
+    // you must have all pieces in home to move outside
+    AllPiecesNotInHome,
+    
     // unknown error
     Unknown,
-    BlockedByEnemy,
-    WrongHomeDirection,
 }
