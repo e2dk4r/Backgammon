39d9f46e2b37f83d1e714339ca88423c2a9e6b92
e2dk4r <43293320+e2dk4r@users.noreply.github.com>
2020-03-17 16:47:15 +0300

    add constants for switching scenes

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -4,4 +4,10 @@ public class Constants
 {
     public const string LAYER_PIECE_STR = "Piece";
     public static int LAYER_PIECE => LayerMask.GetMask(LAYER_PIECE_STR);
+
+    public const string SCENE_GAME = "GameScene";
+
+    public const string PREF_CURRENT_PLAYER = "current_player";
+    public const string PREF_CURRENT_PLAYER1 = "player1";
+    public const string PREF_CURRENT_PLAYER2 = "player2";
 }
