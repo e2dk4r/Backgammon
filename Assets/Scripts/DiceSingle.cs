f50d4a805f8d0401bee28cb34c04a4fbc6370733
e2dk4r <43293320+e2dk4r@users.noreply.github.com>
2020-03-17 15:42:00 +0300

    add chosing who is first scene

---
+++ Assets/Scripts/DiceSingle.cs
@@ -0,0 +1,119 @@
+using UnityEngine;
+
+public class DiceSingle : MonoBehaviour
+{
+    public Sprite[] diceSprites;
+    public int value = 0;
+    public Vector2 direction;
+    public float moveSpeed = 5f;
+    public float rollSpeed = 3f;
+
+    private Rigidbody2D body2D;
+    private SpriteRenderer spriteRenderer;
+
+    private bool animationStarted = false;
+    public bool animationFinished = false;
+
+    private const float ANIMATION_TIME = 1.5f;
+    private const float CHANGE_SPRITE_TIME = .2f;
+    private float animationTime = ANIMATION_TIME;
+    private float changeSpriteTime = CHANGE_SPRITE_TIME;
+    private bool changeSprite = false;
+    private float currentMoveSpeed;
+    private float currentRollSpeed;
+
+    #region Unity API
+
+    private void Awake()
+    {
+        body2D = transform.Find("Value").GetComponent<Rigidbody2D>();
+        spriteRenderer = transform.Find("Value").GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (!animationStarted)
+            return;
+
+        if (!animationFinished)
+        {
+            animationTime -= Time.deltaTime;
+            if (animationTime <= 0)
+            {
+                animationFinished = true;
+                AfterAnimationFinish();
+            }
+
+            currentMoveSpeed -= Time.deltaTime * currentMoveSpeed;
+            currentRollSpeed -= Time.deltaTime * currentRollSpeed;
+        }
+
+        if (!animationFinished && !changeSprite)
+        {
+            changeSpriteTime -= Time.deltaTime;
+            if (changeSpriteTime <= 0)
+            {
+                changeSprite = true;
+            }
+        }
+
+        if (!animationFinished && !changeSprite)
+        {
+            body2D.velocity = new Vector2(direction.x * currentMoveSpeed, direction.y * currentMoveSpeed);
+            body2D.rotation = animationTime * 360 * currentRollSpeed;
+        }
+
+        if (!animationFinished && changeSprite)
+        {
+            DisplayRandom();
+            changeSprite = false;
+            changeSpriteTime = CHANGE_SPRITE_TIME;
+        }
+    }
+
+    #endregion
+
+    #region Draw Methods
+
+    private void DisplayValue()
+    {
+        spriteRenderer.sprite = diceSprites[value - 1];
+    }
+
+    private void DisplayRandom()
+    {
+        spriteRenderer.sprite = diceSprites[UnityEngine.Random.Range(0, diceSprites.Length)];
+    }
+
+    #endregion
+
+    private void AfterAnimationFinish()
+    {
+        body2D.velocity = Vector2.zero;
+        DisplayValue();
+    }
+
+    private void ResetFields()
+    {
+        animationTime = ANIMATION_TIME;
+        animationFinished = false;
+        changeSpriteTime = CHANGE_SPRITE_TIME;
+        changeSprite = false;
+        currentRollSpeed = rollSpeed;
+        currentMoveSpeed = moveSpeed;
+    }
+
+    private void Roll()
+    {
+        value = Random.Range(1, 7);
+    }
+
+    public void RollAgain(Vector2 startPos)
+    {
+        body2D.position = new Vector3(startPos.x, startPos.y, 0);
+        ResetFields();
+        Roll();
+        animationStarted = true;
+    }
+
+}
---
+++ Assets/Scripts/Scene/WhoIsFirst.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WhoIsFirst : MonoBehaviour
+{
+    public DiceSingle dicePlayer1;
+    public DiceSingle dicePlayer2;
+
+    public GameObject infoScreen;
+    public GameObject rollScreen;
+
+    private Vector2 startPos1;
+    private Vector2 startPos2;
+
+    private Button rollButton;
+    private Text infoText;
+    private Text countdownText;
+    private bool countdownStarted = false;
+    private bool countdownFinished = false;
+    private float countdownTimer = 3f;
+
+    private const string INFO_TEXT = "InfoText";
+    private const string COUNTDOWN_TEXT = "CountdownText";
+    private const string ROLL_BUTTON = "RollButton";
+
+    #region Unity API
+
+    private void Awake()
+    {
+        infoText = infoScreen.transform.Find(INFO_TEXT).GetComponent<Text>();
+        countdownText = infoScreen.transform.Find(COUNTDOWN_TEXT).GetComponent<Text>();
+
+        rollButton = rollScreen.transform.Find(ROLL_BUTTON).GetComponent<Button>();
+        rollButton.onClick.AddListener(RollDicesAgain);
+
+        startPos1 = new Vector2(dicePlayer1.transform.position.x, dicePlayer1.transform.position.y);
+        startPos2 = new Vector2(dicePlayer2.transform.position.x, dicePlayer2.transform.position.y);
+    }
+
+    private void Start()
+    {
+        infoScreen.SetActive(false);
+        rollScreen.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (dicePlayer1.animationFinished)
+            AfterDicesRolled();
+
+        if (countdownStarted && !countdownFinished)
+        {
+            countdownTimer -= Time.deltaTime;
+            UpdateCountdownText();
+
+            if (countdownTimer <= 0)
+            {
+                countdownFinished = true;
+                AfterCountdownFinished();
+            }
+        }
+    }
+
+    #endregion
+
+    private void UpdateCountdownText()
+    {
+        countdownText.text = $"{ Mathf.CeilToInt(countdownTimer) }";
+    }
+
+    private void AfterDicesRolled()
+    {
+        if (dicePlayer1.value == dicePlayer2.value)
+        {
+            rollScreen.SetActive(true);
+        }
+        else
+        {
+            infoScreen.SetActive(true);
+            UpdateInfoText();
+            countdownStarted = true;
+        }
+    }
+
+    private void AfterCountdownFinished()
+    {
+        Debug.Log("Switch");
+    }
+
+    private void UpdateInfoText()
+    {
+        if (dicePlayer1.value > dicePlayer2.value)
+            infoText.text = "Player 1 starts in";
+        else
+            infoText.text = "Player 2 starts in";
+    }
+
+    private void RollDicesAgain()
+    {
+        rollScreen.SetActive(false);
+
+        dicePlayer1.RollAgain(startPos1);
+        dicePlayer2.RollAgain(startPos2);
+    }
+}
